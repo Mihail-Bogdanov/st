bdc7844baa115e802746e92134eb1e38a48fb70c
mihail-bogdanov <mihabog0720@gmail.com>
2022-01-14 15:54:04 +0100

    updated config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=13:antialias=true:autohint=true"; 
+static char *font = "Fira Code:pixelsize=14:antialias=true:autohint=true"; 
 static char *font2 = "Fontawesome:pixelsize=15:antialias=true:autohint=false";
 static int borderpx = 10;
 
